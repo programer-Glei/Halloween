f827e1a702bc50af6a8469a9f857b8accc9493b0
Gleibson Pereira <gleibson.santos05@gmail.com>
2021-11-01 21:24:31 -0300

    proximo comit

--- javascript/script.js
+++ javascript/script.js
@@ -45,3 +45,25 @@ let swiper = new Swiper(".home-slider",{
         disableOnInteraction: false
     }
 })
+
+let swiper1 = new Swiper(".products-slider",{
+    spaceBetween: 30,
+    loop: true,
+    centeredSlides: true,
+    autoplay:{
+        delay: 2500,
+        disableOnInteraction: false
+    },
+    grabCursor: true,
+    breakpoints:{
+        0:{
+            slidesPerView: 1,
+        },
+        768:{
+            slidesPerView: 2,
+        },
+        991:{
+            slidesPerView: 3,
+        }
+    }
+})
