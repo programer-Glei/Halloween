ad197335e7283f9ba375c646a997a7440fbac701
Gleibson Pereira <gleibson.santos05@gmail.com>
2021-11-17 16:22:29 -0300

    proximo comit

--- javascript/script.js
+++ javascript/script.js
@@ -12,6 +12,9 @@ document.querySelector('#menu-btn').onclick = () =>{
     searchForm.classList.remove('active')
 }
 
+let section = document.querySelectorAll('section')
+let navLinks = document.querySelectorAll('.header .navbar a')
+
 window.onscroll = () =>{
     searchForm.classList.remove('active')
     navbar.classList.remove('active')
@@ -21,6 +24,20 @@ window.onscroll = () =>{
     }else{
         document.querySelector('.header').classList.remove('active')
     }
+
+    section.forEach(sec =>{
+        let top = window.scrollY
+        let offset = sec.offsetTop - 200
+        let height = sec.offsetHeight
+        let id = sec.getAttribute('id')
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(link =>{
+                link.classList.remove('active')
+                document.querySelector('.header .navbar a[href*='+id+']').classList.add('active')
+            })
+        }
+    })
 }
 
 window.onload = () =>{
